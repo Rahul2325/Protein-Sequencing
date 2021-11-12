7c9dedf9e6fef613ca7b069a61133c8c4c498087
Rahul2325 <rahul230494@gmail.com>
2021-11-12 12:10:07 +0530

    Outline different edges

--- hw6_protein.py
+++ hw6_protein.py
@@ -304,7 +304,17 @@ Parameters: list of strs ; 2D list of values
 Returns: list of strs
 '''
 def makeEdgeList(labels, biggestDiffs):
-    return
+    print(biggestDiffs[0])
+    black=[]
+    first_item=[]
+    for row in biggestDiffs:
+        first_item.append(row[0])
+    for row in labels:
+        if row in first_item:
+            black.append("black")
+        else:
+            black.append("white")
+    return black
 
 
 '''
